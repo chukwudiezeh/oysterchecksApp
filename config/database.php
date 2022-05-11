88192c6462d920b5d4c74aef0487725525af66ca
tmctyneinno <101737151+tmctyneinno@users.noreply.github.com>
2022-05-11 13:50:17 +0100

    mod 1.11

--- config/database.php
+++ config/database.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Str;
 
+$database_url = parse_url(env('JAWSDB_URL'));
+
+$hostname = $database_url['host'];
+$username = $database_url['user'];
+$password = $database_url['pass'];
+$database = ltrim($database_url['path'], '/');
+
+
 return [
 
     /*
@@ -46,11 +54,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $hostname),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
