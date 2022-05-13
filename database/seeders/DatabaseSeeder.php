bbc3eb654988319deaeae394f62504748f17b03e
unknown <cezeh96@yahoo.com>
2022-05-13 03:48:35 +0100

    1.6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,9 +20,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            WalletSeeder::class,
-            AdminSeeder::class,
             InputFieldSeeder::class,
+            AdminSeeder::class,
+            WalletSeeder::class,
             VerificationSeeder::class,
             CandidateServiceSeeder::class,
                         
