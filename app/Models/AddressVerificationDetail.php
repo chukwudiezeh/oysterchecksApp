82bcd76034cab23c620d526f268f5900d413313b
unknown <cezeh96@yahoo.com>
2022-05-25 03:39:36 +0100

    mod 6.1

--- app/Models/AddressVerificationDetail.php
+++ app/Models/AddressVerificationDetail.php
@@ -8,4 +8,68 @@ use Illuminate\Database\Eloquent\Model;
 class AddressVerificationDetail extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'address_verification_id',
+        'reference_id',
+        'candidate',
+        'guarantor',
+        'business',
+        'agent',
+        'address',
+        'status',
+        'task_status',
+        'subject_consent',
+        'start_date',
+        'end_date',
+        'submitted_at',
+        'execution_date',
+        'completed_at',
+        'accepted_at',
+        'revalidation_date',
+        'notes',
+        'is_flagged',
+        'agent_submitted_longitude',
+        'agent_submitted_latitude',
+        'report_geolocation_url',
+        'map_address_url',
+        'submission_distance_in_meters',
+        'reasons',
+        'signature',
+        'images',
+        'building_type',
+        'building_color',
+        'gate_present',
+        'availability_confirmed_by',
+        'closest_landmark',
+        'additional_info',
+        'report_agent_access',
+        'incident_report',
+        'description',
+        'report_id',
+        'download_url',
+        'business_type',
+        'business_id',
+        'yv_user_id',
+        'type',
+        'yv_id',
+        'links',
+    ];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'candidate' => 'object',
+        'business' => 'object',
+        'agent' => 'object',
+        'address' => 'object',
+        'notes' => 'array',
+        'images' => 'array',
+        'links' => 'array'
+    ];
 }
