36e1e304578d222120e5b4e9f502a041ccd86a6a
tmctyneinno <101737151+tmctyneinno@users.noreply.github.com>
2022-06-02 11:02:49 +0100

    mod 9.6

--- app/Jobs/HandleVerificationsYouVerify.php
+++ app/Jobs/HandleVerificationsYouVerify.php
@@ -32,8 +32,8 @@ class HandleVerificationsYouVerify extends SpatieProcessWebhookJob
      */
     public function handle()
     {
-        $webhookCallData = json_decode($this->webhookCall, true)['payload'];
-        logger($webhookCallData);
+        // $webhookCallData = json_decode($this->webhookCall, true)['payload'];
+        dd($this->webhookCall->payload());
         // logger($webhookCall);
         // if (in_array($webhookCallData['data']["type"], ['individual', 'guarantor', 'business'])) {
         //     $get_verification_details = AddressVerificationDetail::where('reference_id', $webhookCallData['referenceId'])->first();
