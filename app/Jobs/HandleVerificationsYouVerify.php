a32f60ed431a9f137a3c455958032d7de6018216
tmctyneinno <101737151+tmctyneinno@users.noreply.github.com>
2022-06-02 14:08:02 +0100

    mod 9.99

--- app/Jobs/HandleVerificationsYouVerify.php
+++ app/Jobs/HandleVerificationsYouVerify.php
@@ -15,15 +15,8 @@ class HandleVerificationsYouVerify extends SpatieProcessWebhookJob
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     // public $webhookCall;
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
 
+    
     /**
      * Execute the job.
      *
