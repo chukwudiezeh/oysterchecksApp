fd89bebeab215c744dc891adabde627535b4252e
tmctyneinno <101737151+tmctyneinno@users.noreply.github.com>
2022-06-03 12:56:38 +0100

    mod 11.0

--- resources/views/users/address/index.blade.php
+++ resources/views/users/address/index.blade.php
@@ -240,14 +240,14 @@
                                 <td>{{$transaction->user->name}}</td>
                                 <td>{{$transaction->fee}}</td>
                                 <td>@if($transaction->addressVerification->status == 'pending') 
-                                     <span class="text-pending">{{$transaction->addressVerification->status}}</span> 
-                                     @elseif($transaction->addressVerification->status == 'completed')
-                                     <span class="text-success"> {{$transaction->addressVerification->status}}</span>
+                                     <span class="badge badge-soft-purple">{{$transaction->addressVerification->status}}</span> 
+                                     @elseif($transaction->addressVerification->status == 'completed' && $transaction->addressVerification->task_status == 'VERIFIED')
+                                     <span class="badge badge-soft-success"> {{$transaction->addressVerification->status}}</span>
                                      @elseif($transaction->addressVerification->status == 'awaiting_schedule')
-                                     <span class="text-success"> {{$transaction->addressVerification->status}}</span>
+                                     <span class="badge badge-soft-dark"> {{$transaction->addressVerification->status}}</span>
                                      @elseif($transaction->addressVerification->status == 'completed' && $transaction->addressVerification->task_status == 'NOT VERIFIED')
-                                     <span class="text-success"> {{$transaction->addressVerification->status}}</span>
-                                     @else <span class="text-danger"> {{$transaction->status}}</span> 
+                                     <span class="badge badge-soft-warning"> {{$transaction->addressVerification->status}}</span>
+                                     @else <span class="badge badge-soft-danger"> {{$transaction->status}}</span> 
                                      @endif 
                                 </td>
                                  <td>{{$transaction->created_at}}</td>
