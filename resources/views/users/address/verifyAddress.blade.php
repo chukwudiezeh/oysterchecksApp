cf486f5eb04635b1a53c0cb4fe36f270b8f925fa
tmctyneinno <101737151+tmctyneinno@users.noreply.github.com>
2022-06-21 15:31:58 +0100

    address pages modifs.

--- resources/views/users/address/verifyAddress.blade.php
+++ resources/views/users/address/verifyAddress.blade.php
@@ -31,72 +31,7 @@
              </div>
              <!--end col-->
          </div>
-         <div class="row ">
-             <div class="col-lg-12">
-                 <div class="row justify-content-center">
-                     <div class="col-md-6 col-lg-4">
-                         <div class="card report-card ">
-                             <div class="card-body" style="background:rgb(36, 16, 82)">
-                                 <div class="row d-flex justify-content-center">
-                                     <div class="col">
-                                         <p class="mb-0 fw-semibold text-white">Successful {{$slug->name}}</p>
-                                         <h3 class="m-0 text-white">{{count($success)}}</h3>
-                                     </div>
-                                     <div class="col-auto align-self-center">
-                                         <div class="report-main-icon bg-light-alt">
-                                             <i data-feather="users" class="align-self-center text-muted icon-sm"></i>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-                             <!--end card-body-->
-                         </div>
-                         <!--end card-->
-                     </div>
-                     <div class="col-md-6 col-lg-4">
-                         <div class="card report-card">
-                             <div class="card-body" style="background:rgb(36, 16, 82)">
-                                 <div class="row d-flex justify-content-center">
-                                     <div class="col">
-                                         <p class="text-white mb-0 fw-semibold">Failed {{$slug->name}}</p>
-                                         <h3 class="m-0 text-white">{{count($failed)}}</h3>
-                                     </div>
-                                     <div class="col-auto align-self-center">
-                                         <div class="report-main-icon bg-light-alt">
-                                             <i data-feather="users" class="align-self-center text-muted icon-sm"></i>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-                             <!--end card-body-->
-                         </div>
-                         <!--end card-->
-                     </div>
-                     <div class="col-md-6 col-lg-4">
-                         <div class="card report-card">
-                             <div class="card-body" style="background:rgb(36, 16, 82)">
-                                 <div class="row d-flex justify-content-center">
-                                     <div class="col">
-                                         <p class="text-white mb-0 fw-semibold">Pending Request</p>
-                                         <h3 class="m-0 text-white">{{count($pending)}}</h3>
-                                     </div>
-                                     <div class="col-auto align-self-center">
-                                         <div class="report-main-icon bg-light-alt">
-                                             <i data-feather="users" class="align-self-center text-muted icon-sm"></i>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-                             <!--end card-body-->
-                         </div>
-                         <!--end card-->
-                     </div>
-                     <!--end col-->
-                     <!--end col-->
-                 </div>
-                 <!--end row-->
-             </div>
-         </div>
+         
          <div class="row">
              <div class="col-lg-12">
                  <div class="card">
