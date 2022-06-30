fd17b2336be4576db162ae128164009c9a68d5ed
tmctyneinno <101737151+tmctyneinno@users.noreply.github.com>
2022-06-30 13:21:27 +0100

    modification

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\User;
+// use App\Models\User;
 
 function moneyFormat($data, $currency){
     $data = number_format($data);
@@ -19,6 +19,39 @@ function GenerateRefs(){
     $id = rand(0000,9999);
     $ref = $ref.$id;
     return $ref;
+}
+
+function executeCurl($data, $host, $method)
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+     CURLOPT_URL => $host,
+     CURLOPT_RETURNTRANSFER => true,
+     CURLOPT_ENCODING => "",
+     CURLOPT_MAXREDIRS => 10,
+     CURLOPT_TIMEOUT => 45,
+     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+     CURLOPT_CUSTOMREQUEST => $method,
+     CURLOPT_POSTFIELDS => $data,
+     CURLOPT_FAILONERROR => 1,
+     CURLOPT_HTTPHEADER => [
+       "Content-Type: application/json",
+       // "Token: zntFmihZ.g9gQAcMzK5st9Mb71uGxqi0H6hI19t3lsNjn"
+       "Token: EAgjeZKG.Hazn4C1dhxI7ehgLJjYhLvJij182Ccc0UCTS"
+     ],
+   ]);
+   
+   $response = curl_exec($curl);
+
+   if(curl_errno($curl)){
+     dd('error:'. curl_errno($curl));
+   }else{
+   $res = json_decode($response, true);
+   
+   curl_close($curl);
+   return $res;
+   }
+
 }
 
 
